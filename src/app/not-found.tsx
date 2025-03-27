e032ce828dcc496bdccc18584edb1ca9aaa70bda
neue.dev <modavid.1964@gmail.com>
2025-03-27 22:34:59 +0800

    404 page.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,24 @@
+import { Title } from '@/components/ui/title'
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <main className="min-h-screen bg-[#1A1A1A] text-white flex flex-col items-center justify-center p-4">
+			{/* Logo */}
+      <Title />
+
+      {/* Thank you message */}
+      <div className="text-center mb-12">
+        <h2 className="text-white-header-1 mb-4">404 - Not Found</h2>
+				<br />
+				<Link href="/" target="_blank" className="hover:opacity-90 transition-opacity">Go back home.</Link>
+      </div>
+
+      {/* Contact info */}
+      <div className="text-center text-sm text-gray-400">
+        Questions or concerns? Call us at:<br />
+        <span className="font-medium text-white">09620900909</span>
+      </div>
+    </main>
+  )
+}
