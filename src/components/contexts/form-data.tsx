05609ca5348fc076441c90ffcd0c8928a892289f
Rinaldo Lee <rinaldochenglee@gmail.com>
2025-03-29 03:26:52 +0800

    update defaults

--- src/components/contexts/form-data.tsx
+++ src/components/contexts/form-data.tsx
@@ -2,78 +2,81 @@ import { createContext } from "react";
 
 export const createFormContext = () => {
 
-	// Default context
-	type Qual = { 
-		key: string, 
-		value: string 
-	}
+  // Default context
+  type Qual = {
+    key: string,
+    value: string
+  }
 
-	let formData: any = [
-		{
-			key: 'monthly salary range',
-			value: 'Php 8000 - Php 15000'
-		}, {
-			key: 'skills',
-			value: 'cooking, cleaning',
-		}
-	];
+  let formData: any = [
+    {
+      key: 'Monthly Salary Range',
+      value: 'Php 8000 - Php 15000'
+    }, {
+      key: 'Skills',
+      value: 'cooking, cleaning',
+    }, {
+      key: 'Agency Fee',
+      value: 'Php 10000 - Php 20000'
+    }
+  ];
 
-	// Returns value of that field
-	const getField = (field: string) => {
-		return formData.filter((f: Qual) => f.key === field)[0]?.value;
-	}
+  // Returns value of that field
+  const getField = (field: string) => {
+    return formData.filter((f: Qual) => f.key === field)[0]?.value;
+  }
 
-	// Updater for form data
-	const setField = (field: string, value: string) => {
+  // Updater for form data
+  const setField = (field: string, value: string) => {
 
-		// Remove existing field
-		const newFormData = formData.filter((f: Qual) => f.key !== field);
-		
-		// Push new value for field
-		newFormData.push({
-			key: field,
-			value: value
-		});
+    // Remove existing field
+    const newFormData = formData.filter((f: Qual) => f.key !== field);
 
-		// Update form data
-		formData = newFormData
-	}
+    // Push new value for field
+    newFormData.push({
+      key: field,
+      value: value
+    });
 
-	// Rename field
-	const renameField = (field: string, newname: string) => {
-		
-		// Remove existing field
-		const oldFieldValue = getField(field);
-		const newFormData = formData.filter((f: Qual) => f.key !== field);
+    // Update form data
+    formData = newFormData
+  }
 
-		// Push new field and new value
-		newFormData.push({
-			key: newname,
-			value: oldFieldValue,
-		})
+  // Rename field
+  const renameField = (field: string, newname: string) => {
 
-		// Update form data
-		formData = newFormData
-	}
+    // Remove existing field
+    const oldFieldValue = getField(field);
+    const newFormData = formData.filter((f: Qual) => f.key !== field);
 
-	// Remove existing field
-	const removeField = (field: string) => {
-		formData = formData.filter((f: Qual) => f.key !== field)
-	}
+    // Push new field and new value
+    newFormData.push({
+      key: newname,
+      value: oldFieldValue,
+    })
 
-	// Gets serialized data, to send to server when done
-	const getAll = (): Qual[] => {
-		return formData;
-	}
-	
-	// Create new context + its data
-	return {
-		getField,
-		setField,
-		getAll,
-		renameField,
-		removeField,
-	}
+    // Update form data
+    formData = newFormData
+  }
+
+  // Remove existing field
+  const removeField = (field: string) => {
+    formData = formData.filter((f: Qual) => f.key !== field)
+  }
+
+  // Gets serialized data, to send to server when done
+  const getAll = (): Qual[] => {
+    return formData;
+  }
+
+  // Create new context + its data
+  return {
+    getField,
+    setField,
+    getAll,
+    renameField,
+    removeField,
+  }
 }
 
 export const FormContext = createContext(createFormContext());
