22576d243e3e5ef4619687ec5de033287ae06c1c
Rinaldo Lee <rinaldochenglee@gmail.com>
2025-03-29 02:35:23 +0800

    fix my big oopsie

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,4 @@
 import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
-import PostgresAdapter from "@/lib/pgadaptor"
-import { dbPool } from "@/lib/db";
-
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-      authorization: {
-      }
-    }),
-    // ...add more providers here
-  ],
-  secret: process.env.AUTH_SECRET,
-  adapter: PostgresAdapter(dbPool)
-}
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
