3bc0b364808b083cb116b5bb027b6a7527be3c32
neue.dev <modavid.1964@gmail.com>
2025-03-29 01:37:42 +0800

    Add favicon + fix err.

--- src/components/ui/contact.tsx
+++ src/components/ui/contact.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 
- * @ Modified time: 2025-03-29 01:08:37
+export function Contact() {
 	const baseClass = 'text-body mt-8 opacity-30 text-white lg:text-right text-center';
 	const animationClass = 'motion-translate-y-in-50 motion-opacity-in-0 motion-delay-500';
 	
