2aa709a02da2d154add7958d9aa5db18e6cdf2c6
neue.dev <modavid.1964@gmail.com>
2025-04-04 23:40:51 +0800

    Create modal API.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,37 @@
 "use client";
 
 import { useState } from "react";
-import { Background } from "@/components/pages/landing/background";
+import { Background } from "@/app/root/background";
 import { Title, Contact } from "@/components/ui/branding";
-import { CardStep1 } from "@/components/pages/landing/card-step-1";
+import { CardStep1 } from "@/app/root/card-step-1";
 import {
   FormContext,
   createFormContext,
 } from "@/components/contexts/form-data";
 import { ProgressIndicator } from "@/components/ui/progress-indicator";
-import { CardStep0 } from "@/components/pages/landing/card-step-0";
-import { CardStep2 } from "@/components/pages/landing/card-step-2";
-import { CardStep3 } from "@/components/pages/landing/card-step-3";
+import { CardStep0 } from "@/app/root/card-step-0";
+import { CardStep2 } from "@/app/root/card-step-2";
+import { CardStep3 } from "@/app/root/card-step-3";
 import Head from "next/head";
 import { RenderSwitch } from "@/components/utils/renderSwitch";
 import { Main } from "@/components/main";
 import { WindowPage, WindowPadding } from "../components/ui/window";
+import { Modal, useModal } from "@/components/ui/modal";
 
 export default function Home() {
   // State of the landing page
+  const testModal = useModal("Hello world");
   const [formContext, setFormContext] = useState(createFormContext());
   const [step, setStep] = useState(0);
   const totalSteps = 4;
 
   // Step updaters
   function nextStep() {
-    console.log("next");
     setStep(Math.min(step + 1, totalSteps));
   }
 
   // Previous step
   function prevStep() {
-    console.log("prev");
     setStep(Math.max(step - 1, 0));
   }
 
@@ -81,6 +81,9 @@ export default function Home() {
         </WindowPage>
 
         <WindowPage></WindowPage>
+        <Modal onAgree={() => alert("blyat")} modal={testModal}>
+          <div>This is an awesome modal.</div>
+        </Modal>
       </Main>
     </>
   );
