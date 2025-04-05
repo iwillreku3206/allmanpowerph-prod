e50b56473bbea171956eea27fdd3eea6184501b3
Rinaldo Lee <rinaldochenglee@gmail.com>
2025-04-06 00:44:39 +0800

    add auto refresh

--- src/app/searches/[id]/protected/page.tsx
+++ src/app/searches/[id]/protected/page.tsx
@@ -52,17 +52,22 @@ export default function Page() {
   const [dialogOpen, setDialogOpen] = useState(false);
   const [disableSubmit, setDisableSubmit] = useState(false);
   const dialogRef = useRef<HTMLDialogElement>(null);
+  const [intervalId, setIntervalId] = useState<number | null>(null);
 
   const id = urlParams.id?.toString();
   if (!id) router.push("/404");
 
+  async function get() {
+    const data = await getData(id as string, page, count);
+    setData(data);
+    setTotalCount(data.length);
+    setFirstLoadDone(true);
+  }
+
   useEffect(() => {
-    async function get() {
-      const data = await getData(id as string, page, count);
-      setData(data);
-      setTotalCount(data.length);
-      setFirstLoadDone(true);
-    }
+    intervalId && clearInterval(intervalId)
+    const interval = setInterval(get, 7000)
+    setIntervalId(interval as unknown as number)
     get();
   }, [page, count, selected]);
 
